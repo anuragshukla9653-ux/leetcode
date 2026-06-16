2e20efa9ceb3f1605c250d12bbe2375c82efc501
Anurag Shukla <anuragshukla9653@gmail.com>
2026-06-16 20:14:45 +0530

    Implement removeDublicatea function to remove duplicates from a sorted array

---
+++ Easy/removeDublicateFromSortedArr26.js
@@ -0,0 +1,16 @@
+let removeDublicatea = function(nums) {
+    let j = 0;
+    for ( let i = 1; i< nums.length; i++ ) {
+
+        if ( nums[i] !==nums[j] ) {
+            j++;
+            nums[j] = nums[i];
+        }
+
+    }
+
+    return j+1;
+
+}
+
+console.log(removeDublicatea([0,0,1,1,1,2,2,3,3,4]));
