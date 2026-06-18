f10e4b0d01e24d4ed02b7e8102a72e94e1d84628
Anurag Shukla <anuragshukla9653@gmail.com>
2026-06-18 23:57:02 +0530

    Implement majorityElement function to find the majority element in an array

---
+++ Easy/majorityElement169.js
@@ -0,0 +1,23 @@
+var majorityElement = function(nums) {
+
+    let map = {};
+    let maxCount = 0;
+    let majorityElement;
+
+    for ( let i = 0; i < nums.length; i++ ) {
+
+        map[nums[i]] = (map[nums[i]] || 0) + 1;
+
+        if(map[nums[i]] > maxCount){
+
+            maxCount = map[nums[i]];
+
+            majorityElement = nums[i];
+        }
+    }
+
+    return majorityElement;
+};
+
+console.log(majorityElement([3,2,3]));
+console.log(majorityElement([2,2,1,1,1,2,2]));
