2e38461389ce0d492d811e2d7b5e9f6bf23bed52
Anurag Shukla <anuragshukla9653@gmail.com>
2026-06-19 23:33:39 +0530

    Refactor majorityElement function for clarity and consistency in counting elements

--- Easy/majorityElement169.js
+++ Easy/majorityElement169.js
@@ -6,12 +6,14 @@ var majorityElement = function(nums) {
 
     for ( let i = 0; i < nums.length; i++ ) {
 
-        map[nums[i]] = (map[nums[i]] || 0) + 1;
+        if ( map.hasOwnProperty(nums[i]) ) {
+            map[nums[i]]++;
+        } else {
+            map[nums[i]] = 1;
+        }
 
-        if(map[nums[i]] > maxCount){
-
+        if ( map[nums[i]] > maxCount ) {
             maxCount = map[nums[i]];
-
             majorityElement = nums[i];
         }
     }
@@ -21,3 +23,24 @@ var majorityElement = function(nums) {
 
 console.log(majorityElement([3,2,3]));
 console.log(majorityElement([2,2,1,1,1,2,2]));
+
+// var majorityElement = function (nums) {
+
+//     let map = {};
+//     let maxCount = 0;
+//     let majorityElement;
+
+//     for ( let i = 0; i < nums.length; i++ ) {
+
+//         map[nums[i]] = (map[nums[i]] || 0) + 1;
+
+//         if(map[nums[i]] > maxCount){
+
+//             maxCount = map[nums[i]];
+
+//             majorityElement = nums[i];
+//         }
+//     }
+
+//     return majorityElement;
+// };
