6d90fdc96a72cbefaf0b8ddb7bcce1ec71ac76ef
Anurag Shukla <anuragshukla9653@gmail.com>
2026-06-21 20:35:31 +0530

    Remove commented-out alternative implementations of singleNumber function for clarity

--- Easy/singleNumber136.js
+++ Easy/singleNumber136.js
@@ -20,3 +20,44 @@ var singleNumber = function( nums ) {
 
 console.log(singleNumber([4,1,2,1,2]));
 console.log(singleNumber([2,2,1]));
+
+// var singleNumber = function ( nums ) {
+    
+//     let map = {};
+
+//     for ( let i = 0; i < nums.length; i++ ) {
+
+//         if ( map.hasOwnProperty ( nums[i] ) ) {
+
+//            delete map [ nums [i] ];
+
+//         } else {
+
+//             map [ nums [i] ] = i;
+
+//         }
+
+//     }
+
+//     return Number(Object.keys(map)[0]);
+
+// };
+
+
+// var singleNumber = function(nums){
+
+//     let map = {};
+
+//     for(let num of nums){
+
+//         map[num] = (map[num] || 0) + 1;
+//     }
+
+//     for(let key in map){
+
+//         if(map[key] === 1){
+
+//             return Number(key);
+//         }
+//     }
+// };
