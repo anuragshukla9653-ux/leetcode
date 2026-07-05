da8e4a5c3b66df18dba9dbdbb514b12e3eb16e40
Anurag Shukla <anuragshukla9653@gmail.com>
2026-07-05 23:37:50 +0530

    Implement searchInsert function to find the insertion position in a sorted array

---
+++ Easy/searchnsertionPortion35.js
@@ -0,0 +1,23 @@
+let searchInsert = function(nums, target) {
+
+    let left = 0;
+    let right = nums.length - 1;    
+    while (left <= right) {
+
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+};
+
+console.log("Search Insertion Position");
+console.log(searchInsert([1,3,5,6], 5));
+console.log(searchInsert([1,3,5,6], 2));
+console.log(searchInsert([1,3,5,6], 7));
+console.log(searchInsert([1,3,5,6], 0));    
